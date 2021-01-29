e7f36c1a5c8b2c261310b3efa082874995e1eab2
Haxman <smartdan112233@gmail.com>
2021-01-29 18:36:07 +0300

    update

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml;
 
 namespace WebApplication1.Controllers
 {
@@ -12,24 +13,13 @@ namespace WebApplication1.Controllers
         {
             return View();
         }
-        /*
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
 
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-        */
         public ActionResult Customers()
         {
-            return View("Customers");
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(AppDomain.CurrentDomain.GetData("DataDirectory")+"/test.xml");
+
+            return View("Customers", xDoc.DocumentElement);
         }
 
         public ActionResult Orders()
---
+++ WebApplication1/Models/DataModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Data.Entity;
+
+namespace WebApplication1.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public DateTime Regdate { get; set; }
+        public DateTime Birdate { get; set; }
+    }
+
+    public class Order
+    {
+        public int id { get; set; }
+        public int cid { get; set; }
+        public int cname { get; set; }
+        public decimal value { get; set; }
+        public DateTime date { get; set; }
+
+
+    }
+
+    public class DataContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
