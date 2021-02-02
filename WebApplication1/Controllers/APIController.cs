bcfb60ecd376ea99331c6d5f79fb234e56cf0198
Haxman <smartdan112233@gmail.com>
2021-02-02 03:22:10 +0300

    now it can write to file and not only read

---
+++ WebApplication1/Controllers/APIController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Tools;
+
+namespace WebApplication1.Controllers
+{
+    public class APIController : Controller
+    {
+        // GET: API
+        public string Index()
+        {
+            return "OOGA BOOGA";
+        }
+        public string SetCustomerName(ControllerContext controllerContext)
+        {
+            if (XmlWorks.setCustomerNameByCid(Request.QueryString.Get("id"), Request.QueryString.Get("param"))) return "OK";
+            return "ERROR";
+        }
+
+        public string SetCustomerPhone(ControllerContext controllerContext)
+        {
+            if (XmlWorks.setCustomerPhoneByCid(Request.QueryString.Get("id"), Request.QueryString.Get("param"))) return "OK";
+            return "ERROR";
+        }
+
+        public string SetCustomerEmail(ControllerContext controllerContext)
+        {
+            if (XmlWorks.setCustomerEmailByCid(Request.QueryString.Get("id"), Request.QueryString.Get("param"))) return "OK";
+            return "ERROR";
+        }
+
+        public string SetCustomerBirdate(ControllerContext controllerContext)
+        {
+            if (XmlWorks.setCustomerBirdateByCid(Request.QueryString.Get("id"), Request.QueryString.Get("param"))) return "OK";
+            return "ERROR";
+        }
+        //////////////////////////////
+        public string SetOrderStatus(ControllerContext controllerContext)
+        {
+            if (XmlWorks.setOrderStatusByOid(Request.QueryString.Get("id"), Request.QueryString.Get("param"))) return "OK";
+            return "ERROR";
+        }
+
+        public string SetOrderRegdate(ControllerContext controllerContext)
+        {
+            if (XmlWorks.setOrderDateByOid(Request.QueryString.Get("id"), Request.QueryString.Get("param"))) return "OK";
+            return "ERROR";
+        }
+
+        public string SetOrderValue(ControllerContext controllerContext)
+        {
+            if (XmlWorks.setOrderValueByOid(Request.QueryString.Get("id"), Request.QueryString.Get("param"))) return "OK";
+            return "ERROR";
+        }
+    }
+}
--- WebApplication1/Tools/XmlWorks.cs
+++ WebApplication1/Tools/XmlWorks.cs
@@ -81,5 +81,143 @@ namespace WebApplication1.Tools
             }
             return "";
         }
+        ////////////////////////////////////
+        public static bool setCustomerNameByCid(string cid, string input)
+        {
+            if (input.Length > 50) return false;
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(AppDomain.CurrentDomain.GetData("DataDirectory") + "/test.xml");
+            XmlElement root = xDoc.DocumentElement;
+            XmlElement customers = (XmlElement)root.GetElementsByTagName("Customers")[0];
+            foreach (XmlElement elem in customers)
+            {
+                if (elem.GetAttribute("cid") == cid) 
+                {
+                    elem.SetAttribute("Name", input);
+                    xDoc.Save(AppDomain.CurrentDomain.GetData("DataDirectory") + "/test.xml");
+                    if (elem.GetAttribute("Name") == input) return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool setCustomerPhoneByCid(string cid, string input)
+        {
+            if (input.Length > 16) return false;
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(AppDomain.CurrentDomain.GetData("DataDirectory") + "/test.xml");
+            XmlElement root = xDoc.DocumentElement;
+            XmlElement customers = (XmlElement)root.GetElementsByTagName("Customers")[0];
+            foreach (XmlElement elem in customers)
+            {
+                if (elem.GetAttribute("cid") == cid)
+                {
+                    elem.SetAttribute("Phone", input);
+                    xDoc.Save(AppDomain.CurrentDomain.GetData("DataDirectory") + "/test.xml");
+                    if (elem.GetAttribute("Phone") == input) return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool setCustomerEmailByCid(string cid, string input)
+        {
+            if (input.Length > 50) return false;
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(AppDomain.CurrentDomain.GetData("DataDirectory") + "/test.xml");
+            XmlElement root = xDoc.DocumentElement;
+            XmlElement customers = (XmlElement)root.GetElementsByTagName("Customers")[0];
+            foreach (XmlElement elem in customers)
+            {
+                if (elem.GetAttribute("cid") == cid)
+                {
+                    elem.SetAttribute("Email", input);
+                    xDoc.Save(AppDomain.CurrentDomain.GetData("DataDirectory") + "/test.xml");
+                    if (elem.GetAttribute("Email") == input) return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool setCustomerBirdateByCid(string cid, string input)
+        {
+            if (input.Length > 10) return false;
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(AppDomain.CurrentDomain.GetData("DataDirectory") + "/test.xml");
+            XmlElement root = xDoc.DocumentElement;
+            XmlElement customers = (XmlElement)root.GetElementsByTagName("Customers")[0];
+            foreach (XmlElement elem in customers)
+            {
+                if (elem.GetAttribute("cid") == cid)
+                {
+                    elem.SetAttribute("Birdate", input);
+                    xDoc.Save(AppDomain.CurrentDomain.GetData("DataDirectory") + "/test.xml");
+                    if (elem.GetAttribute("Birdate") == input) return true;
+                }
+            }
+            return false;
+        }
+
+        ///////////////////////
+
+        public static bool setOrderStatusByOid(string oid, string input)
+        {
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(AppDomain.CurrentDomain.GetData("DataDirectory") + "/test.xml");
+            XmlElement root = xDoc.DocumentElement;
+            XmlElement orders = (XmlElement)root.GetElementsByTagName("Orders")[0];
+            foreach (XmlElement elem in orders)
+            {
+                if (elem.GetAttribute("oid") == oid)
+                {
+                    elem.SetAttribute("Status", input);
+                    xDoc.Save(AppDomain.CurrentDomain.GetData("DataDirectory") + "/test.xml");
+                    if (elem.GetAttribute("Status") == input) return true;
+
+                }
+            }
+            return false;
+        }
+
+        public static bool setOrderValueByOid(string oid, string input)
+        {
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(AppDomain.CurrentDomain.GetData("DataDirectory") + "/test.xml");
+            XmlElement root = xDoc.DocumentElement;
+            XmlElement orders = (XmlElement)root.GetElementsByTagName("Orders")[0];
+            foreach (XmlElement elem in orders)
+            {
+                if (elem.GetAttribute("oid") == oid)
+                {
+                    elem.SetAttribute("Value", input);
+                    xDoc.Save(AppDomain.CurrentDomain.GetData("DataDirectory") + "/test.xml");
+                    if (elem.GetAttribute("Value") == input) return true;
+                }
+
+            }
+            return false;
+        }
+
+        public static bool setOrderDateByOid(string oid, string input)
+        {
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(AppDomain.CurrentDomain.GetData("DataDirectory") + "/test.xml");
+            XmlElement root = xDoc.DocumentElement;
+            XmlElement orders = (XmlElement)root.GetElementsByTagName("Orders")[0];
+            foreach (XmlElement elem in orders)
+            {
+                if (elem.GetAttribute("oid") == oid)
+                {
+                    elem.SetAttribute("Regdate", input);
+                    xDoc.Save(AppDomain.CurrentDomain.GetData("DataDirectory") + "/test.xml");
+                    if (elem.GetAttribute("Regdate") == input) return true;
+                }
+
+            }
+            return false;
+        }
+
+
+
     }
 }
